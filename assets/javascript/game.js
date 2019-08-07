2b87e3027e2b76cc9f0b5fb6e4672dabe0b188fc
Kevin Mahoney <51928306+kevinmahoney135@users.noreply.github.com>
2019-08-06 20:14:11 -0500

    Added some initial Javascript code.

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+var words = ["Motorcycle","Panhead","Scout","Bobber","Softtail","Indian",];
+var guessed = [];
+var word = "";
+
+    // This function is run whenever the user presses a key.
+    document.onkeyup = function(event) {
+
+    }
