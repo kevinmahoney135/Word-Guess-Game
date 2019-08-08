26d74f1d0f9d229a941c7471984361f58d270d84
Kevin Mahoney <51928306+kevinmahoney135@users.noreply.github.com>
2019-08-07 21:27:24 -0500

    Added more code to populate correctly guessed letters.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,8 +10,11 @@ var currentWord;
 var guessCounter = 12;
 var winCounter = 0;
 var letter;
-var solution = "_ ";
+var solution = "";
+var solutionText = "";
+var solutionArray = [];
 var letterMatched = false;
+var wordLength = 0;
 
 var winCountText = document.getElementById("winCount");
 var solutionWordText = document.getElementById("solutionWord");
@@ -32,24 +35,43 @@ function initGame () {
 
     // Initialize the word to play using the getRandomWord function.
     currentWord = getRandomWord();
+    wordLength = currentWord.length;
 
     // Log the answer to the console for deugging
     console.log(currentWord);
 
-    // Initialize the placeholders for the word's letters with underscore characters.
-    for (i = 0; i < currentWord.length; i++)
+    // Initialize the placeholders for the word's letters with
+    // underscore characters.
+    for (var i = 0; i < wordLength; i++)
     {
-        solution = solution + "_ ";
+        solutionArray[i] = "_ ";
     }
 
-    // Initialize the content of the elements that are updated as the game is played.
+    buildSolution();
+
+    // Initialize the content of the elements that are updated
+    // as the game is played.
     winCountText.textContent = winCounter;
-    solutionWordText.textContent = solution;
     guessesLeftText.textContent = guessCounter;
     lettersGuessedText.textContent = guessedLetters;
 }
 
-// Run the initGame fucntion to initialize variables and document elements.
+function buildSolution () {
+    solution = "";
+    solutionText = "";
+
+    for (var i = 0; i < wordLength; i++)
+    {
+        solution = solution + solutionArray[i];
+        console.log(solution);
+        solutionText = solutionText + solutionArray[i] + "  ";
+    }
+
+    solutionWordText.textContent = solutionText;
+}
+
+// Run the initGame fucntion to initialize variables and document
+// elements.
 initGame();
 
 // This function is run whenever the user presses a key.
@@ -57,16 +79,33 @@ document.onkeyup = function(event) {
     // Get letter pressed
     letter = event.key;
 
-    // Update Guessed Letters temp variable and update element content to
-    // display the letters guessed so far.
+    // Update Guessed Letters temp variable and update element
+    // content to display the letters guessed so far.
     guessedLetters = guessedLetters + letter.toUpperCase() + " ";
     lettersGuessedText.textContent = guessedLetters;
 
-    // If the letter doesn't match any in the word, decrement the guessCounter variables
-    // and update the element content to display the number of guesses remaining.
-    if (!letterMatched) {
+    // If the letter doesn't match any in the word, decrement
+    // the guessCounter variables and update the element content
+    // to display the number of guesses remaining.
+    if (currentWord.includes(letter)) {
+        for (var i = 0; i < wordLength; i++) {
+            if (currentWord.charAt(i).toUpperCase() === letter.toUpperCase()) {
+                solutionArray[i] = letter + " ";
+            }
+        }
+
+        buildSolution();
+    }
+    else {
         guessCounter--;
         guessesLeftText.textContent = guessCounter;
+    }
+
+    if (solution.toUpperCase() === currentWord.toUpperCase)
+    {
+        winCounter++;
+        winCountText.textContent = winCounter;
+        initGame();
     }
 
     // If guessCounter drops to zero, game over.
